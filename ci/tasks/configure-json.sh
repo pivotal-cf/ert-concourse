8f49281763440bf2801d44f43a9d9e73695a69ef
virtmerlin <merlin@sense.net>
2016-11-30 16:57:37 -0600

    added sep job for deploy-ert

---
+++ ci/tasks/configure-json.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+set -e
+
+
+# Set Vars
+json_file_path="ert-concourse/json_templates/${pcf_iaas}/${terraform_template}"
+json_file_template="${json_file_path}/ert-template.json"
+json_file="json_file/ert.json"
+
+cp ${json_file_template} ${json_file}
+
+if [[ ! -f ${json_file} ]]; then
+  echo "Error: cant find file=[${json_file}]"
+  exit 1
+fi
+
+
+# Iaas Specific ERT  JSON Edits
+
+case ${pcf_iaas} in
+  "azure")
+   echo "No Specific ERT IaaS Config for ${pcf_iaas} at this time ..."
+  ;;
+  *)
+   echo "Error pcf_iaas:${pcf_iaas} not enabled at this time"
+   exit 1
+  ;;
+esac
