bb7c37e4735364dd532d65577226d0acfa44992c
sreenivasmrpivot <srajaram@pivotal.io>
2017-07-15 09:47:46 -0700

    [147785745] Modified ert blob account name

--- ci/tasks/configure-ert.sh
+++ ci/tasks/configure-ert.sh
@@ -55,11 +55,15 @@ perl -pi -e "s/{{pcf_az_2}}/${pcf_az_2}/g" ${json_file}
 perl -pi -e "s/{{pcf_az_3}}/${pcf_az_3}/g" ${json_file}
 perl -pi -e "s/{{terraform_prefix}}/${terraform_prefix}/g" ${json_file}
 
+# Use prefix to strip down a Storage Account Prefix String
+env_short_name=$(echo ${azure_terraform_prefix} | tr -d "-" | tr -d "_" | tr -d "[0-9]")
+env_short_name=$(echo ${env_short_name:0:10})
+ert_azure_account_name = $(echo ${env_short_name}${azure_account_name})
 
 if [[ "${azure_access_key}" != "" ]]; then
 cat ${json_file} | jq \
   --arg azure_access_key "${azure_access_key}" \
-  --arg azure_account_name "${azure_account_name}" \
+  --arg azure_account_name "${ert_azure_account_name}" \
   --arg azure_buildpacks_container "${azure_buildpacks_container}" \
   --arg azure_droplets_container "${azure_droplets_container}" \
   --arg azure_packages_container "${azure_packages_container}" \
